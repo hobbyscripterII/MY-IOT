c4dcf47580b998caa19f6c33552aaf5ae1c6d1fb
hobbyscripterII <wruomma2@naver.com>
2025-03-16 16:03:12 +0900

    [2025-03-16] UPDATE - MQTT 의존성 주입 - CSS 수정

---
+++ src/main/resources/static/js/mqtt.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+	// mqtt 통신 후 ON/OFF 확인
+	
+	const bluetoothSpeaker = $('#bluetooth-speaker');
+	const lighting         = $('#lighting');
+});
+
+function bluetoothSpeakerControl() {
+	sweetAlert('알림', '블루투스 스피커 컨트롤 이벤트입니다.', 'info');
+}
+
+function lightingControl() {
+	sweetAlert('알림', '조명 컨트롤 이벤트입니다.', 'info');
+}
