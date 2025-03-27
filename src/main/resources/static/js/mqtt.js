613b363758c995bbeec59a0b4bcbc43e8a5eb4d8
hobbyscripterII <wruomma2@naver.com>
2025-03-27 17:13:07 +0900

    [2025-03-27] UPDATE - MQTT 통신 테스트 중

--- src/main/resources/static/js/mqtt.js
+++ src/main/resources/static/js/mqtt.js
@@ -12,25 +12,36 @@ function mqttTestControl() {
 	const client 	= mqtt.connect(brokerUrl);
 	
 	client.on('connect', function() {
-		client.subscribe(topic, function(error) {
-			if(error) {
-				console.log('[MQTT ERROR MESSAGE] ', error);
+		// sub
+		client.subscribe(topic, function(e) {
+			if(e) {
+				console.log('mqtt connect fail..');
+				console.log('detail ⇒ ', e);
+			} else {
+				console.log('mqtt connect success..');
+				console.log('topic  ⇒ ', topic);
+			}
+		});
+		
+		client.publish(topic, 'test', function(e) {
+			if(e) {
+				console.log('mqtt publish fail..');
+				console.log('detail ⇒ ', e);
+			} else {
+				console.log('mqtt publish success..');
 			}
 		});
 	});
 	
 	client.on('message', function(topic, message) {
-		sweetAlertHtml('성공', `토픽 : ${topic}<br>메세지 : ${message}`, 'success');
+		const messageToString = message.toString();
+		console.log(`topic = ${topic} message = ${messageToString}`);
+		
+		sweetAlertHtml('성공', `토픽 : ${topic}<br>메세지 : ${messageToString}`, 'success');
 	});
 	
-	const message = '테스트 메세지';
-
-	client.publish(topic, message, function(e) {
-		if(e) {
-			console.log('[MQTT ERROR MESSAGE] ', error);
-		} else {
-			sweetAlertHtml('성공', `토픽 : ${topic}<br>메세지 : ${message}`, 'success');
-		}
+	client.on('error', function(e) {
+		sweetAlertHtml('실패', `에러 메세지 : ${e}`, 'error');
 	});
 }
 
