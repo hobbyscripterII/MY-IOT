c178a724b1df538c82944dcf8b1c3d4e7376ee96
hobbyscripterII <wruomma2@naver.com>
2025-03-30 16:37:40 +0900

    [2025-03-30] MEMO - LED 센서 제어 테스트

--- src/main/resources/static/js/admin_mqtt_test.js
+++ src/main/resources/static/js/admin_mqtt_test.js
@@ -1,25 +1,26 @@
 $(document).ready(() => {
 	const brokerUrl = 'ws://192.168.0.48:8081';
-	const topic		= '/mqtt/mqtt_test';
+	// const topic		= '/mqtt/mqtt_test';
+	const topic		= '/mqtt/led/blink';
 	const client    = mqtt.connect(brokerUrl);
 
 	client.on('connect', () => {
-		// console.log(`TOPIC CONNECT SUCCESS..`);
+		console.log(`TOPIC CONNECT SUCCESS..`);
 		
 		client.subscribe(topic, (e) => {
 			if(e) {
-				// console.log(`TOPIC SUBSCRIPTION FAIL..`);
-				// console.log(`⇒ ${e}`);
+				console.log(`TOPIC SUBSCRIPTION FAIL..`);
+				console.log(`⇒ ${e}`);
 			} else {
-				// console.log(`TOPIC SUBSCRIPTION SUCCESS..`);
-				// console.log(`⇒ ${topic}`);
+				console.log(`TOPIC SUBSCRIPTION SUCCESS..`);
+				console.log(`⇒ ${topic}`);
 			}
 		});
 	});
 
 	client.on('message', (topic, message) => {
-		// console.log(`MQTT CONNECTION SUCCESS..`);
-		// console.log(`⇒ ${message}`);
+		console.log(`MQTT CONNECTION SUCCESS..`);
+		console.log(`⇒ ${message}`);
 		
 		const json          = JSON.parse(message);
 		const jsonMessage   = json.message;
@@ -29,7 +30,7 @@ $(document).ready(() => {
 		const el = `<tr>
 						<td>${jsonMessage}</td>
 						<td>${jsonTimestamp}</td>
-						<td>${jsonEpoch}</td>
+						<td class="mobile-hide">${jsonEpoch}</td>
 					</tr>`;
 					
 		$('table tbody').append(el);	
